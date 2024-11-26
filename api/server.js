ed7104a1735f813db1f2a3d85598a1e84425326e
Davi Mendes <davimlaudares@gmail.com>
2024-11-26 13:37:58 -0300

    Teste db site 4

--- api/server.js
+++ api/server.js
@@ -2,16 +2,23 @@ const jsonServer = require('json-server');
 const cors = require('cors');
 const path = require('path');
 
+// Cria o servidor
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../db/db.json')); // Caminho ajustado
-const middlewares = jsonServer.defaults();
 
+// Configura o caminho para o db.json
+const router = jsonServer.router(path.join(__dirname, '../db/db.json'));
+
+// Middlewares
 server.use(cors());
-server.use(middlewares);
-server.use(router);
+server.use(jsonServer.defaults());
 
+// Configura as rotas
+server.use('/api', router);
+
+// Define a porta
 const PORT = process.env.PORT || 3001;
 
+// Inicia o servidor
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
