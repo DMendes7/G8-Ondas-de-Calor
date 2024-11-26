aebed443175549ef689ae16b6b1a0f6c84c9f63b
Davi Mendes <davimlaudares@gmail.com>
2024-11-26 14:11:46 -0300

    Teste

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json"); // Caminho correto para o arquivo
+const router = jsonServer.router("db/db.json"); // Caminho correto
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use("/api", router); // Define a rota base como /api
-server.listen(3001, () => {
-    console.log("JSON Server está rodando em http://localhost:3001");
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+    console.log(`JSON Server está rodando em http://localhost:${port}/api`);
 });
